9df332001ce79a420a5d07ddb952bb8bc281a929
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-02-24 19:36:49 -0300

    MessageItem layout

---
+++ components/message/MessageItem.js
@@ -0,0 +1,44 @@
+import Link from 'next/link';
+
+import classes from '@/components/message/MessageItem.module.css';
+
+import Card from '@/components/ui/Card';
+
+function MessageItem(props) {
+	return (
+		<>
+			{props.messages.map((message) => (
+				<Card key={message._id}>
+					<div className={classes.header}>{message.postedAt}</div>
+					<div className={classes.body}>
+						<p>{message.message}</p>
+						<p><i>{message.author}</i></p>
+					</div>
+					<div className={classes.footer}>
+						{message.postUrl.post && (
+							<Link href={message.postUrl.post} className={classes.footerItem} target="_blank">
+								Post
+							</Link>
+						)}
+						{message.postUrl.story && (
+							<Link href={message.postUrl.story} className={classes.footerItem} target="_blank">
+								Story
+							</Link>
+						)}
+					</div>
+				</Card>
+			))}
+		</>
+	);
+}
+
+export default MessageItem;
+
+{
+	/* <div className={classes.footer}>
+<ul className={classes.flexContainer}>
+  <li className={classes.flexItem}>{message.postUrl.post}</li>
+  <li className={classes.flexItem}>{message.postUrl.story}</li>
+</ul>
+</div> */
+}
