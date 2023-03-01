5954ccd6862cb215b97042d5f3ba71cb7167d029
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-03-01 15:58:42 -0300

    footer

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,13 +1,15 @@
 import Header from './Header';
+import Footer from './Footer';
 import classes from './Layout.module.css';
 
 function Layout(props) {
-  return (
-    <div>
-      <Header />
-      <main className={classes.main}>{props.children}</main>
-    </div>
-  );
+	return (
+		<div>
+			<Header />
+			<main className={classes.main}>{props.children}</main>
+			<Footer />
+		</div>
+	);
 }
 
 export default Layout;
