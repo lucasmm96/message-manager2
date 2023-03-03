9c311bae8ee0926a343db25922dbe5a73066a513
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-03-03 11:56:25 -0300

    form component

--- components/ui/Form.js
+++ components/ui/Form.js
@@ -1,7 +1,29 @@
 import classes from './Form.module.css';
 
 function Form(props) {
-  return <p>form</p>;
+	return (
+		<form className={classes.form}>
+			{props.fields.map((field) => (
+				<div className={classes.formGroup}>
+					<label htmlFor={field.name}>{field.label}</label>
+					<input
+						type={field.type}
+						id={field.name}
+						name={field.name}
+						required={field.required ? true : false}
+					/>
+				</div>
+			))}
+			<div className={classes.formContainer}>
+				<button className={classes.formContainerItem} type="submit">
+					Submit
+				</button>
+				<button className={classes.formContainerItem} type="button" id="cancel">
+					Cancel
+				</button>
+			</div>
+		</form>
+	);
 }
 
 export default Form;
