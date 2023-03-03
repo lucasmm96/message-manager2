a77b16e379591d9893457e78454e5ef1b74a7062
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-03-03 18:40:15 -0300

    adjust to send all information needed by Form

--- components/message/edit/MessageEditForm.js
+++ components/message/edit/MessageEditForm.js
@@ -1,6 +1,10 @@
+import { useRouter } from 'next/router';
+
 import Form from '@/components/ui/Form';
 
 function MessageEditForm(props) {
+	const router = useRouter();
+
 	const fields = [
 		{
 			name: 'message',
@@ -39,13 +43,49 @@ function MessageEditForm(props) {
 		},
 	];
 
-	function submitHandler(data) {
-		
+	const structuredData = {
+		_id: props.data._id,
+		message: props.data.message,
+		author: props.data.author,
+		postedAt: props.data.postedAt,
+		urlPost: props.data.urlPost,
+		urlStory: props.data.urlStory,
+	};
+
+	async function submitHandler(data) {
+		const bodyData = [
+			{
+				_id: data._id,
+				message: data.message,
+				author: data.author,
+				postedAt: data.postedAt,
+				postUrl: {
+					post: data.urlPost,
+					story: data.urlStory,
+				},
+			},
+		];
+
+		try {
+			await fetch('http://localhost:3000/message/update', {
+				method: 'POST',
+				body: bodyData,
+			});
+		} catch (error) {}
 	}
-	
+
+	function cancelHandler() {
+		router.push('/');
+	}
+
 	return (
 		<div className="container">
-			<Form fields={fields} data={props.data} onSubmitHandler={submitHandler} />
+			<Form
+				fields={fields}
+				data={structuredData}
+				onSubmitHandler={submitHandler}
+				onCancelHandler={cancelHandler}
+			/>
 		</div>
 	);
 }
