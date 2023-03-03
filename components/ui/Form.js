927e6a26ff86c4eda72c3ad3bde0b293848a9383
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-03-03 15:20:35 -0300

    submit handler

--- components/ui/Form.js
+++ components/ui/Form.js
@@ -1,9 +1,28 @@
+import { useState } from 'react';
 import { useRouter } from 'next/router';
 
 import classes from './Form.module.css';
 
 function Form(props) {
 	const router = useRouter();
+
+	const [formState, setFormState] = useState({
+		_id: props.data._id,
+		message: props.data.message,
+		author: props.data.author,
+		postedAt: props.data.postedAt,
+		urlPost: props.data.urlPost,
+		urlStory: props.data.urlStory,
+	});
+
+	function changeHandler(event) {
+		setFormState({ ...formState, [event.target.name]: event.target.value });
+	}
+
+	function submitHandler(event) {
+		event.preventDefault();
+		console.log(formState);
+	}
 
 	function cancelHandler() {
 		router.push('/');
@@ -19,12 +38,17 @@ function Form(props) {
 						id={field.name}
 						name={field.name}
 						{...(field.required && { required: true })}
-						value={field.data}
+						value={formState[field.name] || ''}
+						onChange={changeHandler}
 					/>
 				</div>
 			))}
 			<div className={classes.formContainer}>
-				<button className={classes.formContainerItem} type="submit">
+				<button
+					className={classes.formContainerItem}
+					type="submit"
+					onClick={submitHandler}
+				>
 					Submit
 				</button>
 				<button
