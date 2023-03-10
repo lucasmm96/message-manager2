a929161d7cd811d0a147075458411fe77c214d45
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-03-10 17:30:07 -0300

    remove iconColor

--- components/ui/IconLink.js
+++ components/ui/IconLink.js
@@ -4,11 +4,7 @@ import classes from './IconLink.module.css';
 
 function IconLink(props) {
 	return (
-		<Link
-			className={classes.iconColor}
-			href={props.href}
-			target={props.newTab ? '_blank' : ''}
-		>
+		<Link href={props.href} target={props.newTab ? '_blank' : ''}>
 			<Icon filename={props.filename} alt={props.alt} w={props.w} h={props.h} />
 			{props.text ? <span className={classes.text}>{props.text}</span> : ''}
 		</Link>
