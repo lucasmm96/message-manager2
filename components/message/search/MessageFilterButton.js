9d3066f1b6be17964d84e6363c59c327c492fb38
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-03-10 11:10:46 -0300

    use generic button

--- components/message/search/MessageFilterButton.js
+++ components/message/search/MessageFilterButton.js
@@ -1,12 +1,15 @@
 import classes from './MessageFilterButton.module.css';
 
+import Button from '@/components/ui/Button';
+
 function MessageFilterButton(props) {
 	const isSelected = props.selected ? classes.selected : '';
-	const buttonClasses = `${classes.button} ${isSelected} containerItem`;
 	return (
-		<button className={buttonClasses} onClick={props.method}>
-			{props.label}
-		</button>
+		<Button
+			classes={`${isSelected} containerItem`}
+			click={props.method}
+			label={props.label}
+		/>
 	);
 }
 
