f05b8086e0db7afdb56d7e77b335266b94c559c9
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-03-16 18:07:28 -0300

    turn form into a more generic component

--- components/ui/Form.js
+++ components/ui/Form.js
@@ -1,90 +1,10 @@
-import { useState } from 'react';
-
 import classes from './Form.module.css';
-import IconLink from './IconLink';
 
 function Form(props) {
-	const [formState, setFormState] = useState({ ...props.data });
-
-	function changeHandler(event) {
-		setFormState({ ...formState, [event.target.name]: event.target.value });
-	}
-
-	function submitHandler(event) {
-		event.preventDefault();
-		props.onSubmitHandler(formState);
-	}
-
-	function cancelHandler(event) {
-		event.preventDefault();
-		props.onCancelHandler();
-	}
-
-	function deleteHandler(event) {
-		event.preventDefault();
-		props.onDeleteHandler(formState);
-	}
-
 	return (
 		<form className={classes.form}>
-			{props.fields.map((field) => (
-				<div key={field.name} className={classes.formGroup}>
-					{field.type === 'url' && formState[field.name] ? (
-						<div>
-							<label htmlFor={field.name}>{field.label}</label>
-							<IconLink
-								href={formState[field.name]}
-								newTab={true}
-								filename="link.svg"
-								alt="Open Link"
-								w={20}
-								h={20}
-							/>
-						</div>
-					) : (
-						<label htmlFor={field.name}>{field.label}</label>
-					)}
-					<input
-						type={field.type}
-						id={field.name}
-						name={field.name}
-						{...(field.required && { required: true })}
-						value={formState[field.name] || ''}
-						onChange={changeHandler}
-					/>
-				</div>
-			))}
-			<div className={classes.formContainer}>
-				{props.saveButton && (
-					<button
-						className={classes.formContainerItem}
-						type="submit"
-						onClick={submitHandler}
-					>
-						Save
-					</button>
-				)}
-				{props.cancelButton && (
-					<button
-						className={classes.formContainerItem}
-						type="button"
-						id="cancel"
-						onClick={cancelHandler}
-					>
-						Cancel
-					</button>
-				)}
-				{props.deleteButton && (
-					<button
-						className={classes.formContainerItem}
-						type="button"
-						id="delete"
-						onClick={deleteHandler}
-					>
-						Delete
-					</button>
-				)}
-			</div>
+			<div className={classes.formGroup}>{props.input}</div>
+			<div className={classes.formContainer}>{props.actions}</div>
 		</form>
 	);
 }
