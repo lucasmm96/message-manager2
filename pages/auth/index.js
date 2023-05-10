a6102f5ef35ec57d7959b1297f9e5964821033d9
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-05-10 18:06:02 -0300

    add login and signup pages

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -1,10 +1,34 @@
+import { useState } from 'react';
+
 import Head from '@/components/layout/CustomHead';
+import Login from '@/components/auth/login';
+import Signup from '@/components/auth/signup';
 
 function Auth() {
+	const [headText, setHeadText] = useState('Login');
+	const [isLoginActive, setIsLoginActive] = useState(true);
+	const [isSignupActive, setIsSignupActive] = useState(false);
+
+	function selectLogin() {
+		setHeadText('Login');
+		setIsLoginActive(true);
+		setIsSignupActive(false);
+	}
+
+	function selectSignup() {
+		setHeadText('Signup');
+		setIsLoginActive(false);
+		setIsSignupActive(true);
+	}
+
 	return (
 		<>
-			<Head title="Auth" />
-			<h1>Auth Page</h1>;
+			<Head title={headText} />
+			<h1>{headText}</h1>
+			<div className="container">
+				{isLoginActive && <Login onSelectSignupHandler={selectSignup} />}
+				{isSignupActive && <Signup onSelectLoginHandler={selectLogin} />}
+			</div>
 		</>
 	);
 }
