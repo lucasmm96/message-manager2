400ba78e088959e214ad35349233d9998bd70838
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-05-10 20:42:13 -0300

    text input

---
+++ components/ui/input/TextInput.js
@@ -0,0 +1,27 @@
+import classes from '@/components/ui/input/TextInput.module.css';
+
+function TextInput(props) {
+	function onChangeHandler(event) {
+		props.onChangeHandler(event);
+	}
+
+	function onBlurHandler(event) {
+		props.onBlurHandler(event);
+	}
+
+	return (
+		<>
+			<input
+				id={props.name}
+				className={classes.input}
+				type={props.type}
+				name={props.name}
+				value={props.value}
+				onChange={onChangeHandler}
+				onBlur={onBlurHandler}
+			/>
+		</>
+	);
+}
+
+export default TextInput;
