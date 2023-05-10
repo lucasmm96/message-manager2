2a3aecd34742901a997b2afa2712e0f20ce08b46
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-05-10 20:42:22 -0300

    input label

---
+++ components/ui/input/InputLabel.js
@@ -0,0 +1,25 @@
+import classes from '@/components/ui/input/InputLabel.module.css';
+
+import IconLink from '@/components/ui/IconLink';
+
+function InputLabel(props) {
+	return (
+		<div>
+			<label className={classes.label} htmlFor={props.htmlFor}>
+				{props.label}
+			</label>
+			{props.type === 'url' && props.value && (
+				<IconLink
+					href={props.value}
+					newTab={true}
+					filename="link.svg"
+					alt="Open Link"
+					w={20}
+					h={20}
+				/>
+			)}
+		</div>
+	);
+}
+
+export default InputLabel;
