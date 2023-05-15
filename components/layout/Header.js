e07d8e0d3b0e0466cc36d4e7e8f3c38411b38dad
Lucas Martins Menezes <lucasma@br.ibm.com>
2023-05-15 18:27:42 -0300

    change button login style

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,7 +1,7 @@
 import { useContext } from 'react';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
-import { useMedia } from 'react-use';
+// import { useMedia } from 'react-use';
 
 import classes from '@/components/layout/Header.module.css';
 
@@ -11,7 +11,7 @@ import IconLink from '@/components/ui/IconLink';
 function MainNavigation() {
 	const auth = useContext(AuthContext);
 
-	const smallScreen = useMedia('(max-width: 768px)', false);
+	// const smallScreen = useMedia('(max-width: 768px)', false);
 	const router = useRouter();
 
 	function authHandler() {
@@ -36,33 +36,15 @@ function MainNavigation() {
 							h={20}
 						/>
 					</li>
-					{smallScreen && (
-						<li className={`${classes.logo} ${classes.right}`}>
-							<IconLink
-								href={'/auth'}
-								newTab={false}
-								filename="bars-solid.svg"
-								alt="Options"
-								w={20}
-								h={20}
-							/>
-						</li>
-					)}
-					{!smallScreen && (
-						<>
-							<li className={classes.right}>
-								<a onClick={authHandler}>
-									{auth.isLoggedIn ? 'Logout' : 'Login'}
-								</a>
-							</li>
-							<li className={classes.right}>
-								<Link href="/add-message">Add New Message</Link>
-							</li>
-							<li className={classes.right}>
-								<Link href="/">All Messages</Link>
-							</li>
-						</>
-					)}
+					<li className={`${classes.login} ${classes.right}`}>
+						<a onClick={authHandler}>{auth.isLoggedIn ? 'Logout' : 'Login'}</a>
+					</li>
+					<li className={classes.right}>
+						<Link href="/add-message">Add New Message</Link>
+					</li>
+					<li className={classes.right}>
+						<Link href="/">All Messages</Link>
+					</li>
 				</ul>
 			</nav>
 		</header>
