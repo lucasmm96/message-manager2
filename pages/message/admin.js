d49bbef29d86ca982847f358b399e7c9f6e3881f
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2023-08-14 17:48:36 -0300

    protect route

--- pages/message/admin.js
+++ pages/message/admin.js
@@ -1,5 +1,20 @@
+import { useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import AuthContext from '@/context/AuthContext';
+import MessageAdmin from '@/components/message/MessageAdmin';
+
 function Admin() {
-  return <p>this is admin...</p>;
+  const router = useRouter();
+  const token = useContext(AuthContext).token;
+
+  useEffect(() => { if (!token) router.replace('/auth/login') }, [token]);
+
+  return (
+    <>
+      {token && <MessageAdmin />}
+      {!token && null}
+    </>
+  );
 }
 
 export default Admin;
