e130f8b23599d13a8ffceb91bf21a651be42282f
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2023-09-12 18:22:06 -0300

    disabled text

--- components/ui/FormInput.js
+++ components/ui/FormInput.js
@@ -39,6 +39,7 @@ function FormInput(props) {
             type={props.type}
             name={props.name}
             value={props.value || ''}
+						disabled={props.disabled || false}
             onChangeHandler={onChangeHandler}
             onBlurHandler={onBlurHandler}
           />
