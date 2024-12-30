b89146a5c06f090d82e85f8943621a20240f38d7
Lucas Martins Menezes <lucasmartinsmenezes@gmail.com>
2024-12-30 14:06:33 -0300

    set default `localhost` when `.env` is not defined

--- utils/httpRequests/get.js
+++ utils/httpRequests/get.js
@@ -1,8 +1,8 @@
 async function get(url, token = null) {
   const postHeaders = { 'Content-Type': 'application/json' };
   if (token) postHeaders.Authorization = 'Bearer ' + token;
-  const response = await fetch(`${process.env.API_URL}${url}`, {
-    method: 'GET',
+  const response = await fetch(`${process.env.API_URL || "http://localhost:3000"}${url}`, {
+    method: "GET",
     headers: postHeaders,
   });
   return response;
